c92c607323c97f555e0b684c8f6156a4e50bb5e1
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-12 17:54:25 -0300

    [CHANGED] Naming Convention

--- Pengin/react-app/src/components/Navbar.js
+++ Pengin/react-app/src/components/Navbar.js
@@ -5,7 +5,7 @@ import Logo from '../assets/Logo.png'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
-    const [selected_option, changeSelection] = useState('Home');
+    const [selectedOption, setOption] = useState('Home');
 
     return (
         <div className='Navbar'>
@@ -15,9 +15,9 @@ function Navbar() {
             </div>
 
             <div className='Options'>
-                <Link className='Option' to='/' onClick={() => changeSelection('Home')} id={(selected_option == 'Home') ? 'selected' : 'unselected'}>Home</Link>
-                <Link className='Option' to='/Queue' onClick={() => changeSelection('Queue')} id={(selected_option == 'Queue') ? 'selected' : 'unselected'}>Queue</Link>
-                <Link className='Option' to='/Settings' onClick={() => changeSelection('Settings')} id={(selected_option == 'Settings') ? 'selected' : 'unselected'}>Settings</Link>
+                <Link className='Option' to='/' onClick={() => setOption('Home')} id={(selectedOption === 'Home') ? 'selected' : 'unselected'}>Home</Link>
+                <Link className='Option' to='/Queue' onClick={() => setOption('Queue')} id={(selectedOption === 'Queue') ? 'selected' : 'unselected'}>Queue</Link>
+                <Link className='Option' to='/Settings' onClick={() => setOption('Settings')} id={(selectedOption === 'Settings') ? 'selected' : 'unselected'}>Settings</Link>
             </div>
 
         </div>
