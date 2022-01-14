722a5e39fb3881a9f9e01bf4ef2243f24c0913fd
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-14 17:32:32 -0300

    [FIXED] Return to default selection on page refresh

--- Pengin/react-app/src/components/Navbar.js
+++ Pengin/react-app/src/components/Navbar.js
@@ -5,7 +5,10 @@ import Logo from '../assets/Logo.png'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
-    const [selectedOption, setOption] = useState('Home');
+    const [selectedOption, setOption] = useState(() => { return sessionStorage.getItem('Tab') || 'Home' });
+
+    // To avoid return to the default navbar selection, we created an arrow function to handle the change in state and save in session storage 
+    const handleOption = (option_selected) => { setOption(option_selected); sessionStorage.setItem('Tab', option_selected) };
 
     return (
         <div className='Navbar'>
@@ -15,9 +18,9 @@ function Navbar() {
             </div>
 
             <div className='Options'>
-                <Link className='Option' to='/' onClick={() => setOption('Home')} id={(selectedOption === 'Home') ? 'selected' : 'unselected'}>Home</Link>
-                <Link className='Option' to='/Queue' onClick={() => setOption('Queue')} id={(selectedOption === 'Queue') ? 'selected' : 'unselected'}>Queue</Link>
-                <Link className='Option' to='/Settings' onClick={() => setOption('Settings')} id={(selectedOption === 'Settings') ? 'selected' : 'unselected'}>Settings</Link>
+                <Link className='Option' to='/' onClick={() => handleOption('Home')} id={(selectedOption === 'Home') ? 'selected' : 'unselected'}>Home</Link>
+                <Link className='Option' to='/Search' onClick={() => handleOption('Search')} id={(selectedOption === 'Search') ? 'selected' : 'unselected'}>Search</Link>
+                <Link className='Option' to='/Settings' onClick={() => handleOption('Settings')} id={(selectedOption === 'Settings') ? 'selected' : 'unselected'}>Settings</Link>
             </div>
 
         </div>
