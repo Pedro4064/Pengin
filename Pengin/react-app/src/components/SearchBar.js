16b03da844914d8c4acbbbf00174c4a1edf3752f
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-14 18:10:13 -0300

    [IMPLEMENTED] Basic Searchbar

---
+++ Pengin/react-app/src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { BiSearch } from 'react-icons/bi'
+import './SearchBar.css'
+
+function SearchBar() {
+
+    const [partialSearch, setPartialSearch] = useState('');
+    const [search, setSearch] = useState('');
+
+    return (
+        <div className='SearchBar'>
+
+            <div className='SearchIcon' onClick={() => setSearch(partialSearch)}>
+                <BiSearch color='#F2E7DC' />
+            </div>
+
+            <div className='SearchBox'>
+                <form onInputCapture={(e) => { setPartialSearch(e.target.value) }} onSubmit={(e) => { e.preventDefault(); setSearch(e.target[0].value) }}>
+                    <input name='Title' placeholder='Title...'></input>
+                </form>
+            </div>
+            <h1>{search}</h1>
+        </div>
+    )
+}
+
+export default SearchBar
