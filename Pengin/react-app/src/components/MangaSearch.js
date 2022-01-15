73a743f24b8a96e78fce6522270fcc1d6706b270
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-14 21:32:59 -0300

    [IMPLEMENTED] Manga Summary Component

---
+++ Pengin/react-app/src/components/MangaSearch.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './MangaSearch.css'
+
+function MangaSearch() {
+    return (
+        <div className='SearchResult'>
+            <img src='https://uploads.mangadex.org/covers/195023bf-cf9a-4772-94ef-09dd6eddea84/61eaa1da-050f-43b7-a8bb-5126a77aac7d.jpg.512.jpg'></img>
+            <div className='SummaryBox'>
+
+                <h3>Komi-SAn</h3>
+
+                <div className='TagBox'>
+                    <div className='Tag'>Comedy</div>
+                    <div className='Tag'>Romance</div>
+                    <div className='Tag'>Slice of Life</div>
+                </div>
+
+                <div className='Summary'>
+                    Komi-san is a beautiful and admirable girl that no one can take their eyes off of. Almost the whole school sees her as the cold beauty that's out of their league, but Tadano Hitohito knows the truth: she's just really bad at communicating with others.
+
+                    Komi-san, who wishes to fix this bad habit of hers, tries to improve it with the help of Tadano-kun by achieving her goal of having 100 friends.
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default MangaSearch
