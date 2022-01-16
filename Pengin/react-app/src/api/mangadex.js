d6334b0620e6487b140571ee429b17cd566ef3d1
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-16 19:18:00 -0300

    [IMPLEMENTED] API calls

---
+++ Pengin/react-app/src/api/mangadex.js
@@ -0,0 +1,32 @@
+async function search_manga(manga_title) {
+    const BASE_URL = `https://api.mangadex.org/manga?title=${manga_title}&limit=20`;
+    const response = await fetch(BASE_URL);
+    const response_json = await response.json();
+
+    return Promise.all(response_json['data'].map(parse_data));
+}
+
+export default search_manga
+
+async function parse_data(api_response) {
+    let manga_data = { Title: '', Id: '', Description: '', Status: '', Tags: [], CoverUrl: '' };
+
+    manga_data.Title = api_response['attributes']['title']['en'];
+    manga_data.Id = api_response['id'];
+    manga_data.Description = api_response['attributes']['description']['en'];
+    manga_data.Status = api_response['attributes']['status'];
+    manga_data.CoverUrl = await get_cover(api_response['relationships'], manga_data.Id);
+    console.log(manga_data);
+    return manga_data;
+
+}
+
+async function get_cover(api_relationships, manga_id) {
+
+    var cover_id = api_relationships.filter((relation) => { if (relation['type'] === 'cover_art') { return true } })[0]['id'];
+    const BASE_URL = `https://api.mangadex.org/cover?ids[]=${cover_id}`
+    const response = await fetch(BASE_URL);
+    const response_json = await response.json()
+    const cover_name = response_json['data'][0]['attributes']['fileName']
+    return `https://uploads.mangadex.org/covers/${manga_id}/${cover_name}.256.jpg`
+}
