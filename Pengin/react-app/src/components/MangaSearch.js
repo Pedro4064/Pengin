bbf4271d713348eab6e932fc684134a5b5cbfe62
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-17 15:28:03 -0300

    [IMPLEMENTED] Tag rendering

--- Pengin/react-app/src/components/MangaSearch.js
+++ Pengin/react-app/src/components/MangaSearch.js
@@ -3,6 +3,19 @@ import { IoIosArrowForward } from 'react-icons/io'
 import './MangaSearch.css'
 
 function MangaSearch({ manga_data }) {
+
+    /**
+     * Slice the tags of a manga to a max of 4.
+     * We need to do so because otherwise the result box would be too polluted and overflow with tags
+     * 
+     * @param {array} tags 
+     * @returns A new array with the first n tags, n being a max of 4 tags
+     */
+    const slice_tags = (tags) => {
+        const sliced_tags = tags.slice(0, Math.min(4, tags.length));
+        return sliced_tags;
+    };
+
     return (
         <div className='SearchResult'>
             <img src={manga_data['CoverUrl']}></img>
@@ -14,9 +27,7 @@ function MangaSearch({ manga_data }) {
                 </div>
 
                 <div className='TagBox'>
-                    <div className='Tag'>Comedy</div>
-                    <div className='Tag'>Romance</div>
-                    <div className='Tag'>Slice of Life</div>
+                    {slice_tags(manga_data['Tags']).map((tag_name) => { return <div className='Tag'>{tag_name}</div> })}
                 </div>
 
                 <div className='Summary'>{manga_data['Description']}</div>
