c03c17427b0612391dc7a48f76be952cd85e0a67
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-18 15:03:14 -0300

    [IMPLEMENTED] Value on search bar

--- Pengin/react-app/src/components/SearchBar.js
+++ Pengin/react-app/src/components/SearchBar.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { BiSearch } from 'react-icons/bi'
-import search_manga from '../api/mangadex'
 import './SearchBar.css'
 
 function SearchBar({ search, setSearch }) {
 
-    const [partialSearch, setPartialSearch] = useState('');
+    const [partialSearch, setPartialSearch] = useState(search);
 
     return (
         <div className='SearchBar'>
@@ -16,7 +15,7 @@ function SearchBar({ search, setSearch }) {
 
             <div className='SearchBox'>
                 <form onInputCapture={(e) => { setPartialSearch(e.target.value) }} onSubmit={(e) => { e.preventDefault(); setSearch(e.target[0].value) }}>
-                    <input name='Title' placeholder='Title...'></input>
+                    <input name='Title' value={partialSearch} onInputCapture={(e) => { setPartialSearch(e.target.value) }} placeholder='Title...'></input>
                 </form>
             </div>
         </div>
