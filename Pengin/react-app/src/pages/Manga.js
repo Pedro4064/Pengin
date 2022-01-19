bd719831e9607b6657481a12e78f79085f02c36d
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-19 01:40:47 -0300

    [IMPLEMENTED] Cover, Title & Author

--- Pengin/react-app/src/pages/Manga.js
+++ Pengin/react-app/src/pages/Manga.js
@@ -13,7 +13,11 @@ function Manga() {
             </div>
 
             <div className='MangaInfo'>
-                <h1>potato</h1>
+                <img className='Thumbnail' src='https://uploads.mangadex.org/covers/0c44cf39-3d6c-4472-815b-9e163613cfe9/5d0929ad-7ce8-4a0a-8d2b-785e310fb549.png'></img>
+                <div className='TextBox'>
+                    <h1>My Wish is to Fall in Love Until You Die</h1>
+                    <h2>Aono Nachi</h2>
+                </div>
             </div>
         </div>
     )
