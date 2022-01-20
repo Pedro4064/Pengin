2ccf9e4cd2ee2a69ffea7387bf9fbe948eff4f20
Pedro4064 <pedrohlcruz@gmail.com>
2022-01-20 02:17:01 -0300

    [IMPLEMENTED] get_overall_data and get_volumes_data

--- Pengin/react-app/src/api/mangadex.js
+++ Pengin/react-app/src/api/mangadex.js
@@ -6,9 +6,53 @@ export default async function search_manga(manga_title) {
     return Promise.all(response_json['data'].map(parse_data));
 }
 
+export async function get_manga_data(manga_id) {
+    var manga_data = { 'Manga info': {}, 'Volumes Data': {} };
+    var data = get_overall_data(manga_id);
+    var volumes_data = get_volumes_data(manga_id);
+
+    Promise.all([data, volumes_data]);
+    manga_data['Manga info'] = data;
+    manga_data['Volumes Data'] = volumes_data;
+
+    console.log('MANGA DATA:', manga_data);
+    return manga_data;
+
+}
+
+const get_overall_data = async (manga_id) => {
+    const BASE_URL = `https://api.mangadex.org/manga/${manga_id}`;
+    const response = await fetch(BASE_URL);
+    const response_json = await response.json();
+
+    return parse_data(response_json['data']);
+}
+
+const get_volumes_data = async (manga_id) => {
+    const BASE_VOLUMES_URL = `https://api.mangadex.org/manga/${manga_id}/aggregate`;
+    const volumes_response = await fetch(BASE_VOLUMES_URL);
+    var volumes_json = await volumes_response.json();
+
+    const BASE_COVERS_URL = `https://api.mangadex.org/cover?order[volume]=asc&manga[]=${manga_id}&limit=100`;
+    const covers_response = await fetch(BASE_COVERS_URL);
+    var covers_json = await covers_response.json();
+
+    return merge_volumes_covers_data(manga_id, volumes_json, covers_json);
+}
+
+const merge_volumes_covers_data = (manga_id, volumes_json, covers_json) => {
+    volumes_json = volumes_json['volumes'];
+    covers_json = covers_json['data'];
+
+    covers_json.map((entry) => { volumes_json[entry['attributes']['volume']]['CoverUrl'] = `https://uploads.mangadex.org/covers/${manga_id}/${entry['attributes']['fileName']}` })
+    return volumes_json;
+
+}
+
 const parse_data = async (api_response) => {
     let manga_data = { Title: '', Id: '', Description: '', Status: '', Tags: [], CoverUrl: '', CoverLargeUrl: '' };
 
+    console.log('PARSING DATA', api_response);
     manga_data.Title = api_response['attributes']['title']['en'];
     manga_data.Id = api_response['id'];
     manga_data.Description = api_response['attributes']['description']['en'];
